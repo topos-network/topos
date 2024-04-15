b2acef1a6da53a0ad959db1a4a7592df13485715
Bastian Gruber <foreach@me.com>
2024-04-15 13:15:50 -0300

    fix: increase next pending interval

--- crates/topos-tce-broadcast/src/task_manager/mod.rs
+++ crates/topos-tce-broadcast/src/task_manager/mod.rs
@@ -107,7 +107,7 @@ impl TaskManager {
     }
 
     pub async fn run(mut self, shutdown_receiver: CancellationToken) {
-        let mut interval = tokio::time::interval(Duration::from_secs(1));
+        let mut interval = tokio::time::interval(Duration::from_millis(100));
 
         loop {
             tokio::select! {
